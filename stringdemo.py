cbd124fdb14929cd86d1fa0836bc0936df2bd8f6
Vijilee-18 <vijileegk2006@gmail.com>
2024-10-18 15:09:39 +0530

    Update stringdemo.py

--- stringdemo.py
+++ stringdemo.py
@@ -1,3 +1,8 @@
+"""
+Author:Vijilee George
+18-10-2006
+Description: stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
+"""
 str_var = "Hello, World!"
 str_another_var = str_var[7:12]
 print(str_another_var)
