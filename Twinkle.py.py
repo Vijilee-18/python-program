abeab827b22205ae90927b81808d939e2fb423af
Vijilee-18 <vijileegk2006@gmail.com>
2024-10-18 22:47:09 +0530

    Add files via upload

---
+++ Twinkle.py.py
@@ -0,0 +1,3 @@
+"""author:Vijilee George kurian
+description: to print the following string in a specific format"""
+print("Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n \t\t Up above the world so high,\n \t\t Like a diamond in the sky.\nTwinkle, twinkle, little star,\t\t\n\tHow I wonder what you are ")
