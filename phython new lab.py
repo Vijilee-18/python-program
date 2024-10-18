a715443456c58250bce78f799d9bd16d33fc4531
Vijilee-18 <vijileegk2006@gmail.com>
2024-10-18 14:39:03 +0530

    Add files via upload

---
+++ phython new lab.py
@@ -0,0 +1,10 @@
+"""
+Author:Vijilee George Kurian
+Date:18-10-2024
+Description:program that ask the user to input their name
+and age
+"""
+name =input("enter your name")
+age =input("enter your age")
+print("hello,",name,"You are",age,"old")
+
