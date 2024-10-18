42b49308ca446487c42ffe3889e004835fda84e4
Vijilee-18 <vijileegk2006@gmail.com>
2024-10-18 15:59:45 +0530

    Add files via upload

---
+++ logical .py
@@ -0,0 +1,11 @@
+"""Author:Vijilee George
+18-10-2006
+Description:demonstrates the usage of arithmetic, comparison, and logical operators. Perform a few operations and print the results."""
+num1 =int(input( " enter num1 :"))
+num2 =int(input("enter num2:"))
+print("sum=",num1+num2,"Division=",num1/num2)
+print("is num1 greater than num2?:",num1>num2)
+print("is num1 equal num2 ?:",num1==num2)
+print(" Logical And :",num1>0 and num2>0 )
+print("logical And:",num1<0 and num2>0 )
+print("logical or :",num1<0  or num2>0)
