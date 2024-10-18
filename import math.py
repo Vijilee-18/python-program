429301e29b69ca2a75c547e4dbf95508db3a0a86
Vijilee-18 <vijileegk2006@gmail.com>
2024-10-18 16:20:25 +0530

    Add files via upload

---
+++ import math.py
@@ -0,0 +1,13 @@
+"""
+Author:Vijilee George
+18-10-2006
+Description:program that uses functions from the math module to perform the following operations on a number provided by the user:
+"""
+import math
+number=int(input("Enter a number "))
+square_root=math.sqrt(number)
+print("square root of number",square_root)
+factorial=math.factorial(number)
+print("factorial of number is :",factorial)
+power=math.pow(5,2)
+print("power is",power)
