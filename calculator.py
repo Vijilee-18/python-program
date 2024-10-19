0b161b19722d4fc1fdbd94ad7952713cd2f668c2
Vijilee-18 <vijileegk2006@gmail.com>
2024-10-19 14:44:37 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,19 @@
+from unicodedata import combining
+
+number_1 = int(input("enter number_1"))
+number_2 = int(input("enter number_2"))
+number_3 = int(input("enter number_3"))
+sum = number_1+number_2
+print("The sum of number_1 and number_2 is:",sum)
+Difference = number_2-number_1
+print("The difference when number_2 is subtracted from number_1",Difference)
+Multiple = number_1*number_2
+print("The product of num1 and num2 is:",Multiple)
+Divide = number_2/number_1
+print("The quotient when num1 is divided by num2 is:",Divide)
+Modulus = number_1%number_2
+print("The remainder when num1 is divided by num2 is:",Modulus)
+result = sum * (number_3/2)
+print("The result of (num1 + num2) * num3 / 2 is:",result)
+
+
